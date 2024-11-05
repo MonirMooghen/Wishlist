d2560a949b441aaaf7fc249c0df1a918347d448f
Vahablotfi <vahablotfi1@gmail.com>
2024-11-05 15:23:03 +0100

    home page created

--- src/main/java/kea/momo/wishlist/controller/ProfileController.java
+++ src/main/java/kea/momo/wishlist/controller/ProfileController.java
@@ -1,9 +1,16 @@
 package kea.momo.wishlist.controller;
 
+import kea.momo.wishlist.model.Profile;
 import kea.momo.wishlist.service.ProfileService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("HomePage")
 public class ProfileController {
 
     private final ProfileService profileService;
@@ -11,5 +18,14 @@ public class ProfileController {
     public ProfileController(ProfileService profileService) {
         this.profileService = profileService;
     }
+
+    // todo this method is just to test the HTML PAGE, must be deleted.
+    @GetMapping("")
+    public String profile(Model model) {
+        //List<Profile> profileLists = profileService.
+        return "homePage";
+    }
+
+
 
 }
