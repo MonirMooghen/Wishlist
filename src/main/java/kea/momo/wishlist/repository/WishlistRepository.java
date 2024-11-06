59b7c7ec1371c35c00d95879b6dd1c59056b23b2
MonirMooghen <momo0003@stud.kea.dk>
2024-11-06 09:49:51 +0100

    CRUD metoder startet og rettet

--- src/main/java/kea/momo/wishlist/repository/WishlistRepository.java
+++ src/main/java/kea/momo/wishlist/repository/WishlistRepository.java
@@ -1,8 +1,23 @@
 package kea.momo.wishlist.repository;
 
+import kea.momo.wishlist.model.Wish;
+import kea.momo.wishlist.model.Wishlist;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class WishlistRepository {
 
+    public List<Wishlist> getAllWishLists() { //TODO rettes
+        return List.of();
+    }
+
+    public void addWish(Wish wish) { //TODO rettes
+    }
+
+    public ArrayList<Wish> getAllWishes() { //TODO rettes
+        return null;
+    }
 }
