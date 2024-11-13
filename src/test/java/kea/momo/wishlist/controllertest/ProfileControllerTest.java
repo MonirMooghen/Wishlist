8060d21eb9fa4a517d6dc662dea4d454f50a00d3
“TobiasBarbosa” <toha0006@stud.kea.dk>
2024-11-13 12:58:36 +0100

    oprettet ProfileControllerTest klasse

---
+++ src/test/java/kea/momo/wishlist/controllertest/ProfileControllerTest.java
@@ -0,0 +1,25 @@
+package kea.momo.wishlist.controllertest;
+
+import kea.momo.wishlist.controller.ProfileController;
+import kea.momo.wishlist.model.Profile;
+import org.junit.jupiter.api.Test;
+
+public class ProfileControllerTest {
+
+    //***ATTRIBUTES***
+    private ProfileController profileController;
+
+    //***TEST***--------------------------------------------------------------------------------------------------------
+    @Test
+    void addProfile(){
+        //arrange
+        Profile profile = new Profile();
+
+        //act
+
+
+        //assert
+    }
+
+    //***END***---------------------------------------------------------------------------------------------------------
+}
